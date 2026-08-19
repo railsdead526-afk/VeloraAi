4ab8be1f94cc7ec60761460a6c7141e653d0177b
railsdead526-afk <railsdead526@gmail.com>
2026-08-19 18:50:18 +0700

    feat: export RAG models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.user import User
 from app.models.conversation import Conversation
 from app.models.message import Message
 from app.models.ai_usage import AIUsage
+from app.models.document import Document, DocumentChunk
