1579ba295278d18bb821ff3654f79ce69669e32a
railsdead526-afk <railsdead526@gmail.com>
2026-08-19 16:25:29 +0700

    feat: add configurable API rate limiter

---
+++ app/core/rate_limit.py
@@ -0,0 +1,11 @@
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+from app.core.config import settings
+
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=[settings.rate_limit_default],
+    storage_uri=settings.rate_limit_storage_uri,
+)
