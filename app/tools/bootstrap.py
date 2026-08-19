420afd36c1ff6cb09985dec0c8006c2c9e3b3361
railsdead526-afk <railsdead526@gmail.com>
2026-08-19 18:41:10 +0700

    feat: register Cloudflare DNS toolset

--- app/tools/bootstrap.py
+++ app/tools/bootstrap.py
@@ -1,4 +1,5 @@
 from app.tools.builtin import register_platform_tools
+from app.tools.cloudflare_builtin import register_cloudflare_tools
 from app.tools.registry import registry
 
 _INITIALIZED = False
@@ -8,5 +9,6 @@ def get_registry():
     global _INITIALIZED
     if not _INITIALIZED:
         register_platform_tools(registry)
+        register_cloudflare_tools(registry)
         _INITIALIZED = True
     return registry
